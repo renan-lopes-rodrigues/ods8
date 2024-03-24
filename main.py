48f6c706b0d8fc60a17dd1d974f354c920911335
Renan Lopes <renan.lopes88@gmail.com>
2024-03-24 12:45:23 -0300

    form start

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    with open('database/custos/fixos.json') as custos_fixos:
-        custos_fixos = json.load(custos_fixos)
-    return render_template('main.html', custos_fixos=custos_fixos)
+    with open('database/custos/custos.json') as custos:
+        custos = json.load(custos)
+    return render_template('main.html', custos=custos)
