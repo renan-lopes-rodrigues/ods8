4ef88d906c40c29568a33c33864e35c01af7bd9a
Renan Lopes <renan.lopes88@gmail.com>
2024-03-24 12:33:02 -0300

    form start

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-"""
-Main file
-"""
+from flask import Flask, render_template
+import json
 
-class Custos:
-    def __init__(self):
-        self.custos: list = list()
+app = Flask(__name__)
 
-    def adiciona_custo_fixo(self, nome: str, valor: float) -> None:
-        print(f"Adicionando custo fixo de {nome}...")
-        novo_custo: dict = {nome: valor}
-        self.custos.append(novo_custo)
-        print(f"Custo Adicionado.")
-
-        return
-
-custos_totais: Custos = Custos()
-tipo_acao: int = int(input("Digite 1 para adicionar custo."))
-
-if tipo_acao == 1:
-    nome_custo: str = input("Digite o nome do custo: ")
-
-    tipo_custo: int = int(input("Digite 1 para custo fixo ou 2 para Custo variável: "))
-
-    if tipo_custo == 1:
-        valor: float = float(input("Digite o valor: "))
-        custos_totais.adiciona_custo_fixo(nome=nome_custo, valor=valor)
-
-
-print(custos_totais.custos)
+@app.route("/")
+def main():
+    with open('database/custos/fixos.json') as custos_fixos:
+        custos_fixos = json.load(custos_fixos)
+    return render_template('main.html', custos_fixos=custos_fixos)
